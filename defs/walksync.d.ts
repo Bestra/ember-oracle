55de62a00a382c2784e73bd01e00c8dcc0f19af7
Chris Westra <cwestra@gmail.com>
2016-03-28 17:30:34 -0700

    add lots of typescript stuff

---
+++ defs/walksync.d.ts
@@ -0,0 +1,6 @@
+
+ 
+
+declare module "walk-sync" {
+  export default function(baseDir: string, _options?): Array<string>;
+}
---
+++ hbs/analyze.ts
@@ -0,0 +1,4 @@
+import * as fs from 'fs'
+import walkSync from 'walk-sync'
+
+walkSync("baseDir")
