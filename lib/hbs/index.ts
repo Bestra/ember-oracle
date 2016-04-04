70f6a210546beb9780f34fb6474327dd5ad1f3e3
Chris Westra <cwestra@gmail.com>
2016-04-03 21:10:25 -0400

    Use mocha to test stuff

--- lib/hbs/index.ts
+++ lib/hbs/index.ts
@@ -9,14 +9,31 @@ import * as fs from 'fs'
 interface Location {
     filePath;
     line;
-    column;
+    column;    
+}
+
+interface YieldedVariableDef {
     
 }
+
+interface BlockParamDef {
+    
+}
+
 export interface Template {
     filePath;
     source;
 }
 
+//block params' context is really the block helper's template
+//if it's not a block param the component's context is the template it's rendered in.
+
+function parseVariableDef(ast: any, name: string, location: Location): YieldedVariableDef | BlockParamDef {
+    
+    
+}
 export default function findDefinition(template: Template, variableName: string, sourceLine: number): Location {
+    
+    
     return {filePath: template.filePath, line: sourceLine, column: 0};
 }
---
+++ test/hbs-test.ts
@@ -0,0 +1,10 @@
+
+var assert = require('assert')
+describe("it works", function() {
+    it("really works", function() {
+        console.log("uuh")
+        assert.ok(false, "it wont' work")
+        assert.ok(true, "it does work");
+        
+    })
+});
