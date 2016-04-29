db5e8b2ffa6a429e7fe21d3a3ac4981c55171ae3
Chris Westra <cwestra@gmail.com>
2016-04-29 12:22:06 -0400

    update transforms

--- codeshift-transforms/test.js
+++ codeshift-transforms/test.js
@@ -2,15 +2,21 @@ export default function transformer(file, api) {
   const j = api.jscodeshift;
   const {expression, statement, statements} = j.template;
 
-  const createNewAssert = function ({node}) {
-    console.log(`${file.path}:${node.loc.start.line}`);
+  const createNotFound = function ({node}) {
+    console.log(`${file.path}:${node.loc.start.line} old notFound assertion`);
     let a = node.arguments[0].object.arguments;
     let b = node.arguments[1];
     return expression`assert.elementNotFound(${a},${b})`;
   };
 
+   const createFound = function ({node}) {
+    console.log(`${file.path}:${node.loc.start.line} old found assertion`);
+    let a = node.arguments[0].object.arguments;
+    let b = node.arguments[1];
+    return expression`assert.elementFound(${a},${b})`;
+  };
   
-  return j(file.source)
+  let mod1 = j(file.source)
     .find(j.Identifier, {name: 'notOk'})
     .closest(j.CallExpression, {arguments:
                                 [{object:
@@ -20,7 +26,18 @@ export default function transformer(file, api) {
                                   },
                                  property: {name: "length"}},
                                  {type: "Literal"}]})
-    .replaceWith(createNewAssert)
+    .replaceWith(createNotFound)
+    .getAST()
   
+  return j(mod1).find(j.Identifier, {name: 'ok'})
+    .closest(j.CallExpression, {arguments:
+                                [{object:
+                                  {callee:
+                                   {name: "find"
+                                   }
+                                  },
+                                 property: {name: "length"}},
+                                 {type: "Literal"}]})
+    .replaceWith(createFound)
     .toSource({quote: 'single', wrapColumn: 50});
 }
