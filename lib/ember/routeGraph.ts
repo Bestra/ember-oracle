12f7eca40dc6937740d62e4ddfc4fc9d7aeb2e7d
Chris Westra <cwestra@gmail.com>
2016-06-10 09:00:11 -0400

    Start looking at route tree

--- lib/ember/routeGraph.ts
+++ lib/ember/routeGraph.ts
@@ -1,4 +1,5 @@
 import * as recast from 'recast'
+import * as _ from 'lodash'
 
 interface RouteNode {
     parent: RouteNode;
@@ -7,20 +8,72 @@ interface RouteNode {
     name: string;
 }
 
-export default function findRoutes(routerAst): RouteNode {
-    recast.visit(routerAst, {
-        visitCallExpression: function(path) {
-          let node: ESTree.CallExpression = path.node;
-          this.traverse(path);
-
+function findChildRoutes(routeFnNode: ESTree.FunctionExpression, parentRoute) {
+    let routeNodes = [];
+    let isRoute = _.matches(
+        {
+            callee: {
+                object: { type: "ThisExpression" },
+                property: { name: "route" }
+            }
         }
+    )
+    recast.visit(routeFnNode, {
+        visitCallExpression(path) {
+            if (isRoute(path.node) && path.node !== parentRoute.node) {
+                routeNodes.push(path.node)
+                return false;
+            } else {
+                this.traverse(path)
+            }
+        }
+    });
+
+    return routeNodes.map((routeCallNode) => {
+        let child = {
+            node: routeCallNode,
+            parent: parentRoute,
+            children: [],
+            moduleName: routeCallNode.arguments[0].value,
+            name: routeCallNode.arguments[0].value
+        }
+        child.children = findChildRoutes(routeCallNode, child)
+        console.log("found a child of ", parentRoute.moduleName, "- ", child.moduleName);
+        
+        return child;
     })
-    
+}
+
+export default function findRoutes(routerAst): RouteNode {
+    let routeFn;
+    let isRouteMap = _.matches(
+        {
+            callee: {
+                object: { name: "Router" },
+                property: { name: "map" }
+            }
+        }
+    )
+
+    recast.visit(routerAst, {
+        visitCallExpression(path) {
+            let node = path.node;
+            if (isRouteMap(node)) {
+                routeFn = node;
+            }
+            return false
+        }
+    });
+
     let appRoute = {
+        node: routeFn,
         parent: null,
-        children: [],
+        children: null,
         moduleName: 'application',
         name: 'application'
     }
+
+    appRoute.children = findChildRoutes(routeFn, appRoute);
+
     return appRoute;
 }
--- test/routeTest.ts
+++ test/routeTest.ts
@@ -9,24 +9,30 @@ describe.only('the route tree', function () {
         });
 
         Router.map(function() {
-        this.route('dashboard', function() {});
-        this.route('widget-factory', function() {});
+        this.route('dashboard', function() {
+            this.route('edit');
+        });
+        this.route('widget-factory');
         });
         `
-        let ast = findRoutes(parseJs(src))
+        let ast = parseJs(src)
         it('has a top application route and children', function () {
             let root = findRoutes(ast);
             assert.equal(root.name, 'application')
             assert.equal(root.children.length, 2)
+            let editRoute = root.children[0];
+            assert.equal(editRoute.children.length, 1)
         })
+        
 
     })
     describe('nested routes', function () {
+        it('generates index routes for each nested route')
 
     })
 
     describe('resources', function () {
-
+        it('detects legacy nested resources too')
     })
 
 })
