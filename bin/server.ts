46e12caa6d7ee3275ed4992bcfd4f4583ae3371f
Chris Westra <cwestra@gmail.com>
2016-08-14 17:13:12 -0400

    read addon paths from .ember-analyzer file

--- bin/server.ts
+++ bin/server.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import startServer from '../lib/server'
 
 function start() {
--- lib/server/startApp.ts
+++ lib/server/startApp.ts
@@ -1,11 +1,28 @@
 import * as path from 'path';
+import * as fs from 'fs';
 import * as resolver from '../util/resolver'
 import * as registry from '../util/registry'
 import { ok } from 'assert'
 
-export default function init(appPath: string, enginePaths: string[] = []) {
-    ok(path.isAbsolute(appPath), "app root must be an absolute path");
-    resolver.setRootPath(appPath);
+export default function init(rootPath: string, enginePaths: string[] = []) {
+    ok(path.isAbsolute(rootPath), "root must be an absolute path");
+    resolver.setRootPath(rootPath);
     registry.registerAppModules();
+    new ConfigFile(rootPath).addonPaths.forEach(p => registry.registerModules(p, "pods"));
     enginePaths.forEach(p => registry.registerModules(p, "pods"));
+}
+
+class ConfigFile {
+    addonPaths;
+    constructor(rootPath: string) {
+        let dotFilePath = path.join(rootPath, ".ember-analyzer");
+        this.addonPaths = [];
+        console.log('looking for config file at ${dotFilePath}');
+        if (fs.existsSync(dotFilePath)) {
+            let dotInfo = JSON.parse(fs.readFileSync(dotFilePath, 'utf8'))
+            console.log("addon paths found in config file:");
+            console.log(dotInfo.addonPaths);
+            this.addonPaths = dotInfo.addonPaths;
+        } 
+    }
 }
