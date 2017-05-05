7052493235eac71cabacb50160d3cd80e2c378ce
Chris Westra <cwestra@gmail.com>
2017-05-05 16:46:20 -0400

    Find parent templates; add license and comments

--- emacs/ember-oracle.el
+++ emacs/ember-oracle.el
@@ -1,5 +1,45 @@
-(require 'json)
+;;; ember-oracle.el --- Emacs interface to the ember-oracle server
+;;
+;; Author: Chris Westra
+;; URL: http://github.com/bestra/ember-oracle
+;;
+;;;;;;;;;;;;;;;;
+;;;; MIT License
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;; This minor mode provides key bindings for navigating an ember-cli
+;; project.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require 'json)
+(require 'cl)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Custom Variables
 (defgroup ember-oracle nil
   "Minor mode for the ember-oracle server"
   :prefix "ember-oracle-"
@@ -21,6 +61,16 @@
       (define-key ember-oracle-mode-keymap value 'ember-oracle-command-prefix))
     (set-default 'ember-oracle-keymap-prefix value)))
 
+(defcustom ember-oracle-completion-system 'default
+  "Which completion system ember-oracle-mode should use."
+  :group 'ember-oracle
+  :type '(radio
+          (const :tag "Helm" helm)
+          (const :tag "Ido" ido)
+          (const :tag "Default" default)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Low-level fns for server interaction
 (defun eo-create-query-string (args)
   (mapconcat (lambda (arg)
                (concat (url-hexify-string (car arg))
@@ -44,17 +94,61 @@
         (kill-buffer (current-buffer)))
       resp))
 
+(defun eo--parse-response (json)
+  "Parses a json-formatted http response"
+  (let ((json-object-type 'plist))
+    (json-read-from-string json)))
+
 (defun eo-find-alternate-file ()
   "gets the alternate file"
   (eo-http-get "files/alternate" `(("path" . ,(buffer-file-name)))))
 
+(defun eo-find-parent-templates ()
+  "gets the parent templates for a given file from the server.
+returns a vector of filenames formatted 'path:line:column'"
+  (eo--parse-response
+   (eo-http-get "templates/parents" `(("path" . ,(buffer-file-name))))))
+
+;;;;;;;;;;;;;
+;;;; Completion
+
+(defun eo--completing-read (question matches)
+  "A smarter completing-read which poses QUESTION with matches being MATCHES.
+This replacement uses a completion system according to
+`ember-oracle-completion-system'."
+  (cond
+   ((eq ember-oracle-completion-system 'ido)
+    (ido-completing-read question matches))
+   ((and (eq ember-oracle-completion-system 'helm)
+         (fboundp 'helm-comp-read))
+    (helm-comp-read question matches
+                    :must-match t))
+   (t (completing-read question matches))))
+
+;;;;;;;;;;;;;;;;;;;;;;
+;;;; Interactive fns
 (defun eo-goto-alternate-file ()
   "switches the current buffer to the alternate file"
   (interactive)
   (find-file (eo-find-alternate-file)))
 
+(defun eo-show-parent-templates ()
+  "dislplays a list of parent templates"
+  (interactive)
+  (let* ((candidates (seq-into (eo-find-parent-templates) 'list))
+         (choice (eo--completing-read "Select a template to view "
+                                      candidates))
+         (parts (split-string choice ":")))
+    (cl-destructuring-bind (file line column) parts
+      (find-file file)
+      (goto-line (string-to-number line))
+      (move-to-column (string-to-number column)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Keyboard Commands
 (defvar ember-oracle-commands-map (make-sparse-keymap))
 (define-key ember-oracle-commands-map (kbd "f a") #'eo-goto-alternate-file)
+(define-key ember-oracle-commands-map (kbd "f p") #'eo-show-parent-templates)
 
 (defvar ember-oracle-mode-keymap (make-sparse-keymap))
 
